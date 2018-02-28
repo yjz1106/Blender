da51a62a52b47844a41b25f656f84caec0386362
WitsTiny <tinyliu@wistronits.com>
2018-02-28 10:55:58 +0800

    Initial Commit

---
+++ Blender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blender
+//
+//  Created by admin on 2018/2/28.
+//  Copyright © 2018年 WITS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Blender/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blender
+//
+//  Created by admin on 2018/2/28.
+//  Copyright © 2018年 WITS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
